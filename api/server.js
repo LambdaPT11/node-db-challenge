3643032cc9d577bd133a9487d7dc442e1ee2eab7
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2020-04-20 21:20:50 -0400

    complete of resource crud. project and tasks still to do

--- api/server.js
+++ api/server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 
-const ProjectRouter = require('../router/project');
+// const ProjectRouter = require('./projects/project-router.js');
+// const TaskRouter = require('./tasks/task-router.js');
+const ResourceRouter = require('../router/resource-router.js');
 
 const server = express();
 
 server.use(express.json());
-server.use('/api/project', ProjectRouter);
-
-server.get('/', (req, res) => {
-    res.status(200).json({ msg: 'server is at API' });
-});
+// server.use('/api/projects', ProjectRouter);
+// server.use('/api/tasks', TaskRouter);
+server.use('/api/resources', ResourceRouter);
 
 module.exports = server;
